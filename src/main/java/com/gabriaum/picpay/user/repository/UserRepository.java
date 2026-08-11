79cdfc096d56aec96dc88e3d16e12a8300925f26
gabriaum <gaabrielbruck@gmail.com>
2026-08-11 16:41:20 -0300

    feat: add findByCpf method and existsByCpfOrEmail check to UserRepository

--- src/main/java/com/gabriaum/picpay/user/repository/UserRepository.java
+++ src/main/java/com/gabriaum/picpay/user/repository/UserRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByCpf(String cpf);
     Optional<UserEntity> findByUsername(String username);
     Optional<UserEntity> findByEmail(String email);
+
+    Boolean existsByCpfOrEmail(String cpf, String email);
 }
