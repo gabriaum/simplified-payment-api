1febc43c44009b2367b68c2627e91d7c6e43f394
gabriaum <gaabrielbruck@gmail.com>
2026-08-11 16:02:13 -0300

    feat: add User record with fields for id, username, email, cpf, role, and balance

---
+++ src/main/java/com/gabriaum/picpay/user/User.java
@@ -0,0 +1,14 @@
+package com.gabriaum.picpay.user;
+
+import com.gabriaum.picpay.user.enums.Role;
+
+import java.math.BigDecimal;
+
+public record User(
+        Long id,
+        String username,
+        String email,
+        String cpf,
+        Role role,
+        BigDecimal balance
+        ) {}
