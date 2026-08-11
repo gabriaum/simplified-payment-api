21b2c2f1610d574e3eb9f3205e02a3588e241c85
gabriaum <gaabrielbruck@gmail.com>
2026-08-11 15:45:12 -0300

    initialize project structure with Spring Boot and MySQL configuration

---
+++ src/test/java/com/gabriaum/picpay/PicpaySimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriaum.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplifiedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
