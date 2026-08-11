f3a108d4f11c9f7ba7997e292e70530a554f4272
gabriaum <gaabrielbruck@gmail.com>
2026-08-11 16:02:05 -0300

    feat: add Role enum for user roles

---
+++ src/main/java/com/gabriaum/picpay/user/enums/Role.java
@@ -0,0 +1,20 @@
+package com.gabriaum.picpay.user.enums;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    USER("Usuário"),
+    SHOPKEEPER("Lojista"),
+    ;
+
+    private final String name;
+
+    @JsonValue
+    public String getName() {
+        return name;
+    }
+}
