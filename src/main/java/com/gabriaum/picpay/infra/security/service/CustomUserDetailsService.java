f1861bbf23eb90250f2f1d196cc4c93db6e300b0
gabriaum <gaabrielbruck@gmail.com>
2026-08-11 16:13:23 -0300

    feat: implement CustomUserDetailsService for user authentication

---
+++ src/main/java/com/gabriaum/picpay/infra/security/service/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.gabriaum.picpay.infra.security.service;
+
+import com.gabriaum.picpay.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+}
