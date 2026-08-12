d9a103558acc1d1e105658dcad9f2686ebfbf8d7
gabriaum <gaabrielbruck@gmail.com>
2026-08-12 01:43:45 -0300

    feat: update RegisterDTO to use firstName and lastName instead of username

--- src/main/java/com/gabriaum/picpay/user/dto/RegisterDTO.java
+++ src/main/java/com/gabriaum/picpay/user/dto/RegisterDTO.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record RegisterDTO(
-        @NotBlank String username,
+        @NotBlank String firstName,
+        @NotBlank String lastName,
         @NotBlank String email,
         @NotBlank String cpf,
         @NotBlank @Size(min = 8, max = 100, message = "A senha deve ter no mínimo 8 caracteres") String password
