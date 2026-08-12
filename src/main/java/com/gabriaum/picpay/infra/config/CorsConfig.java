66be53e5d55847705361d22e29cb1cfa807f14a7
gabriaum <gaabrielbruck@gmail.com>
2026-08-12 01:44:42 -0300

    feat: add CorsConfig for configuring CORS settings in the application

---
+++ src/main/java/com/gabriaum/picpay/infra/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.gabriaum.picpay.infra.config;
+
+import org.jspecify.annotations.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
