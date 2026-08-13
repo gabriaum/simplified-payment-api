07a51bc038e38622738ce7492c0a461a7ca79475
gabriaum <gaabrielbruck@gmail.com>
2026-08-13 02:42:58 -0300

    feat: add exception handler for HttpMessageNotReadableException to validate Role type

--- src/main/java/com/gabriaum/picpay/user/exception/UserExceptionHandler.java
+++ src/main/java/com/gabriaum/picpay/user/exception/UserExceptionHandler.java
@@ -1,8 +1,13 @@
 package com.gabriaum.picpay.user.exception;
 
+import com.gabriaum.picpay.user.enums.Role;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import tools.jackson.databind.exc.InvalidFormatException;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class UserExceptionHandler {
@@ -16,5 +21,22 @@ public class UserExceptionHandler {
     public ResponseEntity<?> userAlreadyExistsException(UserAlreadyExistsException e) {
         return ResponseEntity.status(409)
                 .body(e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadable(HttpMessageNotReadableException exception) {
+        Throwable cause = exception.getCause();
+
+        if (cause instanceof InvalidFormatException invalidFormatException && invalidFormatException.getTargetType() == Role.class)
+            return ResponseEntity.badRequest().body(
+                    Map.of(
+                            "message", "Tipo de conta inválido (Role)",
+                            "allowed", Role.values()
+                    )
+            );
+
+        return ResponseEntity.badRequest().body(
+                Map.of("message", "Corpo da requisição inválido")
+        );
     }
 }
